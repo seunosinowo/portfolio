8cf9182ea8c6d3ca28b414e1c3ce370028ba5428
seunosinowo <oluwaseunpaul98@gmail.com>
2025-06-13 22:09:49 +0100

    Setting up Typescript packages

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <div className="">
+        <h1 className="text-red-300">Seun is my name</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
